2644b1d2bb995ad681e2962099010c1665cb5f83
varchar <50389150+varmeh07@users.noreply.github.com>
2025-02-23 09:59:55 -0500

    Update math.py
    Added rectangle overlapping

--- math.py
+++ math.py
@@ -34,6 +34,16 @@ def hypot(x,y):
     c=sqr_rt(x*x+y*y)
     return c
 
+def is_in_rectangle(x1,x2,y1,y2,a,b):
+    z1=a
+    z2=b
+    print(x1,x2,y1,y2,a,b)
+    #if (z1 in range(x1,x2,1) and z2 in range(y1,y2,1)):
+    if (x1<=a and a<=x2 and y1<=b and b<=y2):
+        return True
+    else: 
+        return False
+
 if __name__ == '__main__':
     num=125
     x,y=1,2
@@ -46,3 +56,30 @@ if __name__ == '__main__':
     print(f"the hypotenus of {x} and {y} = {hypotenus}",)
     distance_2d=hypot(x-a,y-b)
     print(f"the distnace in 2d is = {distance_2d}",)
+    
+    '''
+        rectangle overlaping
+    '''
+    rectangle2=[1,3,1,3]
+    rectangle1=[2,4,2,4]
+    i=0
+    flag=False
+    #g=2
+    for i in range(2):
+        j=2
+        while j<4:
+            if is_in_rectangle(rectangle1[0],rectangle1[1],rectangle1[2],rectangle1[3],rectangle2[i],rectangle2[j]):
+                print(i,j)
+                print('Overlapping')
+                flag=True
+                break
+            else:
+                #print(i,j)
+                print('Not Overlapping')
+            j+=1
+
+    i+=1
+    if(flag):
+        print("the rectangle coordinates are overlapping")
+    else:
+        print("no overlapping try another set")
